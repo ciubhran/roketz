ea1ebfd201468930d3182908c65431eab3f4fbf5
Llaw <ciubhran@gmail.com>
2018-11-04 00:21:20 +0100

    Added audio and added lerp to camera movement.

--- src/client/js/Game.js
+++ src/client/js/Game.js
@@ -28,7 +28,9 @@ class Game extends React.Component {
                 create: this.initGame,
                 update: this.updateGame
             },
-            reactParent: this
+            audio: {
+                disableWebAudio: true
+            }
         };
 
         this.game = new Phaser.Game(config);
@@ -42,11 +44,17 @@ class Game extends React.Component {
         this.load.image('space', 'assets/galaxies/space.png');
         this.load.image('bullet', 'assets/projectiles/bullet.png');
         this.load.image('ship', 'assets/ships/ship64.png');
+        this.load.audio('battle', 'assets/audio/battle.mp3');
     }
 
     initGame() {
         //  A spacey background
         this.space = this.add.image(this.game.config.width / 2, this.game.config.height / 2, 'space');
+
+        this.cameraPos = {x: 0, y: 0};
+
+        this.music = this.sound.add('battle');
+        //this.music.play({ loop: true });
 
         // Setup bullets
         let Bullet = new Phaser.Class({
@@ -101,25 +109,51 @@ class Game extends React.Component {
 
         this.ship = this.physics.add.image(this.game.config.width / 2, this.game.config.height / 2, 'ship').setDepth(2);
 
-        // Ship variables
-        this.lastShot = 0;              // Used to monitor weapon cooldown.
-        this.shipSpeed = 300;           // The default ship speed.
+        const ships = new Map();
 
-        this.shipDrag = 50;             // A force that slows down the velocity of the object.
-        this.shipAngularDrag = 30;      // A force that slows down the rotation of the object.
-        this.shipMaxVelocity = 200;     // The maximum velocity of the ship. Duh.
+        // Slow ship
+        ships.set('FREIGHTER', {
+            drag: 50,
+            angularDrag: 30,
+            maxVelocity: 100,
+            speed: 50,
+            mass: 500
+        });
+
+        // Fast ship
+        ships.set('HALCYON', {
+            drag: 200,
+            angularDrag: 10,
+            maxVelocity: 300,
+            speed: 400,
+            mass: 50
+        });
+
+        // Current ship settings
+        const shipSettings = ships.get('HALCYON');
+
+        // Ship variables
+        this.lastShot = 0;                               // Used to monitor weapon cooldown.
+        this.lastWarp = 0;                               // Used to monitor warp cooldown.
+        this.shipSpeed = shipSettings.speed;             // The default ship speed.
+
+        this.shipDrag = shipSettings.drag;               // A force that slows down the velocity of the object.
+        this.shipAngularDrag = shipSettings.angularDrag; // A force that slows down the rotation of the object.
+        this.shipMaxVelocity = shipSettings.maxVelocity; // The maximum velocity of the ship. Duh.
+        this.shipMass = shipSettings.mass;               // The mass of the object. Affects velocity changes when bumping into other objects.
 
         this.ship.setDrag(this.shipDrag);
         this.ship.setAngularDrag(this.shipAngularDrag);
         this.ship.setMaxVelocity(this.shipMaxVelocity);
+        this.ship.setMass(this.shipMass);
 
         //  Game input
         this.cursors = this.input.keyboard.createCursorKeys();
 
         this.fire = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE);
         this.warp = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.W);
-        
-        this.cameras.main.startFollow(this.ship);
+
+        this.cameras.main.startFollow(this.ship, false, 0.1, 0.1);
     }
 
     updateGame(time, delta) {
@@ -134,7 +168,8 @@ class Game extends React.Component {
         if (this.cursors.up.isDown) {
             this.physics.velocityFromRotation(this.ship.rotation, this.shipSpeed, this.ship.body.acceleration);
         } else {
-            this.ship.setAcceleration(0);
+            this.ship.setAccelerationX(0);
+            this.ship.setAccelerationY(0);
         }
 
         let BULLET = 0x1;
@@ -188,38 +223,46 @@ class Game extends React.Component {
             return {x, y};
         };
 
-        this.engageWarp = (warpPower, delta) => {
+        //  this.cameras.main.pan(x, y, fullDuration / maxRuns, 'Linear', true, (camera, progress, x, y) => {});
+
+        this.engageWarp = (warpPower, instant = false) => {
             if (this.shipTween) {
                 this.shipTween.stop();
+                this.shipTween = null;
             }
 
             const coords = this.getNewCoordinates(this.ship, warpPower);
             const tweenDuration = Math.sqrt(Math.pow(warpPower, 2)) / this.shipSpeed * 1000;
 
+            let shipRemnant = this.physics.add.staticSprite(this.ship.body.x, this.ship.body.y, 'ship');
+            shipRemnant.rotation = this.ship.rotation;
+
+            //this.ship.setVelocity(this.shipMaxVelocity);
+
+            this.tweens.add({
+                targets: shipRemnant,
+                alpha: 0,
+                duration: 1000,
+                onComplete: () => {
+                    shipRemnant.destroy();
+                }
+            });
+
             this.shipTween = this.tweens.add({
                 targets: this.ship,
                 x: this.ship.body.x + coords.x,
                 y: this.ship.body.y + coords.y,
-                duration: tweenDuration
+                duration: instant ? 1 : tweenDuration
             });
         };
 
-        if (this.warpIsDown && this.warp.isUp && this.warpPower > 50) {
-            this.warpIsDown = false;
-            this.engageWarp(this.warpPower, delta);
-            this.warpPower = 0;
-        } else if (!this.warpIsDown && this.warp.isDown) {
-            this.warpPower = 0;
-            this.warpIsDown = true;
-        } else if(this.warp.isUp) {
-            this.warpPower = 0;
-            this.warpIsDown = false;
-        } else if (this.warpIsDown) {
-            this.warpPower += delta;
+        if (this.warp.isDown && time > this.lastWarp) {
+            this.engageWarp(150, true);
+            this.lastWarp = time + 1000;
         }
 
-        this.space.tilePositionX += this.ship.body.deltaX() * 0.5;
-        this.space.tilePositionY += this.ship.body.deltaY() * 0.5;
+        //this.space.tilePositionX += this.ship.body.deltaX() * 0.5;
+        //this.space.tilePositionY += this.ship.body.deltaY() * 0.5;
     }
 
     render() {
